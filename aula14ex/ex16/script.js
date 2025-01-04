420f19acb978f38645912fca5cd666c953799f1f
Rodrigo-dev261 <rodrigo_stringari@hotmail.com>
2025-01-04 14:24:05 -0300

    update

---
+++ aula14ex/ex16/script.js
@@ -0,0 +1,14 @@
+function verificar() {
+    var inicio = document.getElementsByName('nuni')
+    var fim = document.getElementsByName('numf')
+    var passo = document.getElementsByName('nump')
+    var res = document.querySelector('div#res')
+    if (passo == 0) {
+        window.alert('Passo invalido! Considerando PASSO 1')
+    } else {
+        
+    }
+
+    
+}
+
